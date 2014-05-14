d230c4a1ecf8a7d9c870aed909f20de027623126
Feng Chen <fchen3@gmail.com>
2014-05-13 19:52:05 -0500

    new file:   ../../openacc/c/matmul_acc.c 	new file:   ../../openacc/c/matmul_dy_1d.c 	renamed:    dynamic_2d_array.c -> dynamic_2d_array.h 	modified:   matmul_dynamic2d_answer.c

--- matmul/serial/c/matmul_dynamic2d_answer.c
+++ matmul/serial/c/matmul_dynamic2d_answer.c
@@ -1,11 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-             
-typedef double real;
-
-real **allocate_dynamic_2d_array(int nrows, int ncols);
-void free_dynamic_2d_array(real** array_dynamic);
-void print_matrix(real** array_dynamic, int nrows, int ncols, char* fmt_string);
+#include "dynamic_2d_array.h"             
 
 int main() {
     int i,j,k;
@@ -63,30 +58,4 @@ int main() {
     free_dynamic_2d_array(a);
     free_dynamic_2d_array(b);
     free_dynamic_2d_array(c);
-}
-
-real** allocate_dynamic_2d_array(int nrows, int ncols) {
-    /* here is the method to correct the non-contiguous memory problem */
-    int i;
-    real** array_dynamic=(real**)malloc(nrows*sizeof(real*));
-    array_dynamic[0]=(real*)malloc(nrows*ncols*sizeof(real));
-    for (i=1; i<nrows; i++){
-        array_dynamic[i]=array_dynamic[0]+ncols*i;
-    }
-    return array_dynamic;
-}
-
-void free_dynamic_2d_array(real** array_dynamic){
-    free((void*)array_dynamic[0]);
-    free((void*)array_dynamic);
-}
-
-void print_matrix(real** array_dynamic, int nrows, int ncols, char* fmt_string) {
-    int i,j;
-    for (i = 0; i < nrows; i++){
-        for (j = 0; j < ncols; j++){
-            printf(fmt_string, array_dynamic[i][j]);
-        }
-        printf("\n");
-    }
 }
