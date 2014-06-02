273f54f485bb264beb808b5414878e161395b831
Feng Chen <fchen3@gmail.com>
2014-06-02 14:33:05 -0500

    modified:   bm_laplace.sh 	renamed:    subg.pbs -> submit_laplace_acc.pbs

--- laplace/openacc/bm_laplace.sh
+++ laplace/openacc/bm_laplace.sh
@@ -1,5 +1,15 @@
-# benchmark the openacc version of the jacobi iteration
+#benchmark the openacc version of the jacobi iteration
 #!/bin/bash -x
+#PBS -q workq
+#PBS -A hpc_train_2014
+#PBS -l nodes=1:ppn=16
+#PBS -l walltime=00:10:00
+#PBS -o output_s
+#-e eg
+#PBS -j oe
+#PBS -N acc_laplace
+
+cd $PBS_O_WORKDIR
 pgcc -fast laplace_openacc_v0.c
 ./a.out
 pgcc -fast -mp laplace_openacc_v0.c
