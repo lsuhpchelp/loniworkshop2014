90526bd7ea8cf12a48dee2e7d529dbcd84b07ee2
Feng Chen <fchen3@gmail.com>
2014-06-02 20:58:10 -0500

    new file:   saxpy_1stexample.c

---
+++ saxpy/openacc/exercise/saxpy_1stexample.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+
+void saxpy(int n, float a, float *x, float *restrict y) {
+#pragma acc kernels
+    for (int i = 0; i < n; ++i)
+        y[i] = a*x[i] + y[i];
+}
+
+int main(int argc, char **argv)
+{
+    int n = 1<<20; // 1 million floats
+
+    float *x = (float*)malloc(n*sizeof(float));
+    float *y = (float*)malloc(n*sizeof(float));
+    for (int i = 0; i < n; ++i) {
+        x[i] = 2.0f;
+        y[i] = 1.0f;
+    }
+    saxpy(n, 3.0f, x, y);
+    free(x);
+    free(y);
+    return 0;
+}
+
