7a46b437f1d9858181cfc792ccf8ab0fd5e87b48
Feng Chen <fchen3@gmail.com>
2015-05-30 22:26:15 -0500

    modified:   matmul/openacc/solution/mm_acc_v0.c 	modified:   matmul/openacc/solution/submit_mm.pbs

--- matmul/openacc/solution/mm_acc_v0.c
+++ matmul/openacc/solution/mm_acc_v0.c
@@ -64,18 +64,6 @@ int main(int argc, char** argv) {
     // check if the acc version matches the serial version
     if (check) {
         start_time = omp_get_wtime();
-//        #pragma omp parallel shared(a,b,cs), private(i,j,k) 
-//        #pragma omp parallel for
-//        for (i = 0; i < nra; i++){
-//            for (k = 0; k < ncb; k++){
-//                sum = 0.0;
-//                for (j = 0; j < nca; j++){
-//                    sum += a[i][j] * b[j][k];
-//                }
-//                cs[i][k] = sum;
-//            }
-//        }
-
         int    omp_threads;
 #pragma omp parallel shared(a,b,cs) private(i,j,k) 
         {
